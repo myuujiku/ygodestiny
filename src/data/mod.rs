3a46987392989314a201bc0b120f7edac437a1d9
myuujiku <myujiku@proton.me>
2023-11-07 12:40:45 +0100

    remove redb_tables module and add a separate database path for each former table

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -51,7 +51,11 @@ pub mod files {
     lazy_path!(DATA_DB, dirs::ROOT.join("data.db"));
     lazy_path!(DATA_DB_VERSION, dirs::ROOT.join("version.txt"));
 
-    lazy_path!(USER_DB, dirs::USER.join("user.redb"));
+    lazy_path!(DECK_REDB, dirs::USER.join("decks.redb"));
+    lazy_path!(GAME_REDB, dirs::USER.join("games.redb"));
+    lazy_path!(GAME_MODE_REDB, dirs::USER.join("game_modes.redb"));
+    lazy_path!(PACK_REDB, dirs::USER.join("packs.redb"));
+    lazy_path!(SET_REDB, dirs::USER.join("sets.redb"));
 }
 
 pub mod images {
@@ -64,16 +68,4 @@ pub mod images {
 
         Image::from_file(filename)
     }
-}
-
-pub mod redb_tables {
-    use redb::TableDefinition;
-
-    macro_rules! create_tables {
-        ( $( $n:ident ),* ) => { $(
-            pub const $n: TableDefinition<u128, &str> = TableDefinition::new(stringify!($n));
-        )* };
-    }
-
-    create_tables!(DECKS, GAMES, GAME_MODES, PACKS, SETS);
 }
