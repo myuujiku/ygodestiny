a2aba47969a9b7c0ee417062b448655b8fc1dd8c
myuujiku <myujiku@proton.me>
2023-11-07 12:40:45 +0100

    implement RedbStorage for GameMode

--- src/userdata/game_mode/mod.rs
+++ src/userdata/game_mode/mod.rs
@@ -1,5 +1,15 @@
-use crate::traits::RedbStorage;
+use std::path::Path;
 
+use serde::{Deserialize, Serialize};
+
+use crate::data::files;
+use crate::userdata::RedbStorage;
+
+#[derive(Serialize, Deserialize)]
 pub struct GameMode;
 
-impl RedbStorage for GameMode {}
+impl RedbStorage for GameMode {
+    fn db_path() -> &'static Path {
+        &files::GAME_MODE_REDB
+    }
+}
