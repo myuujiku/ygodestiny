3d69e58cdd9d509a01cd9e5762ae5eae26b5944c
myuujiku <myujiku@proton.me>
2023-11-07 12:40:45 +0100

    initialize relm application in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use adw::prelude::*;
+use relm4::prelude::*;
+
+use ygodestiny::data::dirs;
+use ygodestiny::gui;
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+    dirs::init()?;
+
+    let app = relm4::main_adw_application();
+    app.set_application_id(Some("xyz.ygodestiny.YGODestiny"));
+    app.set_resource_base_path(Some("/xyz/ygodestiny/YGODestiny/"));
+
+    let app = RelmApp::from_app(app);
+    app.run::<gui::Main>(());
+
+    Ok(())
 }
