e5ce6945b9bf35f875cd01c66c6c1fee5060b60a
myuujiku <myujiku@proton.me>
2023-11-07 12:40:45 +0100

    add new game mode template

--- src/gui/templates/mod.rs
+++ src/gui/templates/mod.rs
@@ -1,9 +1,11 @@
+pub mod breakpoint;
+
 mod clamp;
+mod new_game_mode;
 mod page;
 mod split_view;
 
-pub mod breakpoint;
-
 pub use clamp::Clamp;
+pub use new_game_mode::NewGameMode;
 pub use page::Page;
 pub use split_view::SplitView;
---
+++ src/gui/templates/new_game_mode.rs
@@ -0,0 +1,64 @@
+use adw::prelude::*;
+use relm4::prelude::*;
+
+use crate::gui::templates::Clamp;
+
+#[relm4::widget_template(pub)]
+impl WidgetTemplate for NewGameMode {
+    view! {
+        adw::ToolbarView {
+            #[name = "header_bar"]
+            add_top_bar = &adw::HeaderBar {
+                #[name = "back_button"]
+                pack_start = &gtk::Button {
+                    set_icon_name: "go-previous",
+                    set_visible: false,
+                },
+            },
+
+            #[template]
+            #[wrap(Some)]
+            set_content = &Clamp {
+                #[name = "carousel"]
+                adw::Carousel {
+                    set_interactive: false,
+
+                    #[name = "types"]
+                    adw::StatusPage {
+                        set_hexpand: true,
+                        set_title: "Select a Game Mode type",
+
+                        #[wrap(Some)]
+                        set_child = &adw::PreferencesGroup {
+                            #[name = "type_timeline"]
+                            adw::ActionRow {
+                                set_title: "Timeline",
+                                set_subtitle: "Progress through sets in a fixed order",
+                                set_activatable: true,
+                                add_suffix: &gtk::Image::builder().icon_name("go-next").build(),
+                            },
+
+                            adw::ActionRow {
+                                set_title: "Paths (WIP)",
+                                set_subtitle: "Travel paths that lead to different sets",
+                            },
+
+                            adw::ActionRow {
+                                set_title: "Chaos (WIP)",
+                                set_subtitle: "Try to make the best out of a pile of random packs",
+                            },
+
+                            adw::ActionRow {
+                                set_title: "Shop (WIP)",
+                                set_subtitle: "Buy packs with Duel Credits",
+                            },
+                        },
+                    },
+
+                    #[name = "settings"]
+                    adw::ToolbarView {},
+                },
+            },
+        }
+    }
+}
