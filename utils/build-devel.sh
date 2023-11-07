54b2c56e6783e523feacb49e2332780b8b6eda2d
myuujiku <myujiku@proton.me>
2023-11-07 12:40:45 +0100

    add util scripts

---
+++ utils/build-devel.sh
@@ -0,0 +1,1 @@
+flatpak-builder --user flatpak/app flatpak/xyz.ygodestiny.YGODestiny.Devel.json --force-clean
---
+++ utils/run.sh
@@ -0,0 +1,1 @@
+flatpak-builder --run flatpak/app flatpak/xyz.ygodestiny.YGODestiny.Devel.json ygodestiny
