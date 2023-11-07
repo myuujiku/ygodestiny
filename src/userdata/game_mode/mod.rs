386f25eaf87d5f0fa5513a15f07331c311e614a1
myuujiku <myujiku@proton.me>
2023-11-07 12:40:45 +0100

    add state to GameMode

--- src/userdata/game_mode/mod.rs
+++ src/userdata/game_mode/mod.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::time::SystemTime;
 
 use serde::{Deserialize, Serialize};
 
@@ -6,10 +7,21 @@ use crate::data::files;
 use crate::userdata::RedbStorage;
 
 #[derive(Serialize, Deserialize)]
-pub struct GameMode;
+pub struct GameMode {
+    metadata: GameModeMetadata,
+    games: Vec<u128>,
+    settings: (),
+}
 
 impl RedbStorage for GameMode {
     fn db_path() -> &'static Path {
         &files::GAME_MODE_REDB
     }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GameModeMetadata {
+    pub name: String,
+    pub description: String,
+    pub last_played: SystemTime,
 }
