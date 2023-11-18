76206eb9916eb30b0780eb733d6b792ca5c7da91
myuujiku <myujiku@proton.me>
2023-11-18 16:10:41 +0100

    remove settings macro

--- ygodestiny_macros/src/lib.rs
+++ ygodestiny_macros/src/lib.rs
@@ -3,11 +3,6 @@ mod subclass;
 use proc_macro::TokenStream;
 
 #[proc_macro]
-pub fn settings(input: TokenStream) -> TokenStream {
-    TokenStream::new()
-}
-
-#[proc_macro]
 pub fn object_subclass(input: TokenStream) -> TokenStream {
     subclass::object_subclass(input)
 }
